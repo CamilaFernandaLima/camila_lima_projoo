23e8ee0500099e8a4c2c023f239ba1cfe39929f9
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2026-04-07 22:49:33 -0300

    Exercício 1

---
+++ Program.cs
@@ -0,0 +1,105 @@
+using System;
+
+// 1. Implementação do Singleton
+public class ConfiguracaoGlobal
+{
+    private static ConfiguracaoGlobal instance;
+
+    public string NomeApp = "Sistema de Notificações";
+    public string Servidor = "smtp.faculdade.edu.br";
+    public int MaxTentativasReenvio = 3;
+
+    //contrutor privado (evitar 'new')
+    private ConfiguracaoGlobal() { }
+
+    public static ConfiguracaoGlobal GetInstance()
+    {
+        if(instance == null)
+        {
+            instance = new ConfiguracaoGlobal();
+        }
+        return instance;
+    }
+}
+
+// 2. Implementação da interface de notificações
+public interface INotifica
+{
+    void Enviar(string mensagem);
+}
+
+public class Email : INotifica
+{
+    public void Enviar(string mensagem)
+    {
+        var configuracao = ConfiguracaoGlobal.GetInstance();
+        Console.WriteLine("Enviando email: " + mensagem);
+    }
+}
+
+public class SMS : INotifica
+{
+    public void Enviar(string mensagem)
+    {
+        Console.WriteLine("Enviando SMS: " + mensagem);
+    }
+}
+
+public class Push : INotifica
+{
+    public void Enviar(string mensagem)
+    {
+        Console.WriteLine("Enviando Push: " + mensagem);
+    }
+}
+
+// 3. Construção do padrão Factory 
+public class FactoryNotificacao
+{
+    public static INotifica Criar(string tipo)
+    {
+        if (tipo == "email")
+        {
+            return new Email();
+        }
+        if (tipo == "sms")
+        {
+            return new SMS();
+        }
+        if (tipo == "push")
+        {
+            return new Push();
+        }
+        else
+        {
+            Console.WriteLine("Erro: esse tipo de notificação não existe no sistema.");
+            return null;
+        }
+    }
+}
+
+// 4. Programa Main
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Iniciando o sistema...");
+
+
+        //testes do singleton
+        var teste1 = ConfiguracaoGlobal.GetInstance();
+        teste1.NomeApp = "App Modificado";
+
+        var teste2 = ConfiguracaoGlobal.GetInstance();
+        Console.WriteLine("Nome do App no teste2: " + teste2.NomeApp);
+
+        Console.WriteLine("Enviando Mensagens...");
+
+        INotifica email1 = FactoryNotificacao.Criar("email");
+        email1.Enviar("Olá, sua nota está no sistema");
+
+        INotifica sms1 = FactoryNotificacao.Criar("sms");
+        sms1.Enviar("Olá, sua nota foi alterada");
+
+    }
+}
